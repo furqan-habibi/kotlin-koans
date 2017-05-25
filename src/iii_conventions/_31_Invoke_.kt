b1bebb51b82784eace162ff4b85bb8e1d88fc4a7
Furqan Habibi <furqan.habibi@isid.co.jp>
2017-05-25 20:08:20 +0900

    30番目から41番目まで

--- src/iii_conventions/_31_Invoke_.kt
+++ src/iii_conventions/_31_Invoke_.kt
@@ -3,7 +3,16 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable {
+    var numOfInvocations = 0
+
+    operator fun invoke(): Invokable {
+        numOfInvocations++
+        return this
+    }
+
+    fun getNumberOfInvocations() = numOfInvocations
+}
 
 fun todoTask31(): Nothing = TODO(
     """
@@ -14,6 +23,6 @@ fun todoTask31(): Nothing = TODO(
     references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    //todoTask31()
+    return invokable()()()().getNumberOfInvocations()
 }
--- src/v_builders/_39_HtmlBuilders.kt
+++ src/v_builders/_39_HtmlBuilders.kt
@@ -22,7 +22,7 @@ fun todoTask39(): Nothing = TODO(
 fun renderProductTable(): String {
     return html {
         table {
-            tr {
+            tr(getTitleColor()) {
                 td {
                     text("Product")
                 }
@@ -34,7 +34,20 @@ fun renderProductTable(): String {
                 }
             }
             val products = getProducts()
-            todoTask39()
+            //todoTask39()
+            products.forEachIndexed { index, (description, price, popularity) ->
+                tr {
+                    td(getCellColor(index, 0)) {
+                        text(description)
+                    }
+                    td(getCellColor(index, 1)) {
+                        text(price)
+                    }
+                    td(getCellColor(index, 2)) {
+                        text(popularity)
+                    }
+                }
+            }
         }
     }.toString()
 }
